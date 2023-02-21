a5a4bd8044c67c812174226b07bc3158faf55a15
David Kane <david@dkane.net>
2023-02-21 23:22:22 +0000

    update cascs

--- cascs/fetch_cascs.py
+++ cascs/fetch_cascs.py
@@ -3,6 +3,7 @@ import csv
 import hashlib
 import io
 import json
+from collections import defaultdict
 
 from pyexcel_ods3 import get_data
 from requests_html import HTMLSession
@@ -11,11 +12,14 @@ from utils import to_titlecase
 CASC_BASE = "https://www.gov.uk/government/publications/community-amateur-sports-clubs-casc-registered-with-hmrc--2"
 CASC_ID_LOOKUP = "cascs_id_lookup.csv"
 CASC_ORG_ID_PREFIX = "GB-CASC"
-RECORD_KEYS = ["name", "address", "postcode"]
+RECORD_KEYS = ["name", "address", "postcode", "active"]
 
 
 with open(CASC_ID_LOOKUP, "r") as f:
-    id_lookups = {r["new_id"]: r["old_id"] for r in csv.DictReader(f)}
+    id_lookups = {
+        **{r["new_id"]: r["old_id"] for r in csv.DictReader(f)},
+        **{r["old_id"]: r["new_id"] for r in csv.DictReader(f)},
+    }
 
 
 def get_org_id(record, org_id_prefix=CASC_ORG_ID_PREFIX):
@@ -78,8 +82,13 @@ def main():
         description="Extract a list of Community Amateur Sports Clubs from HMRC and add a unique identifier"
     )
     parser.add_argument(
+        "infile",
+        type=argparse.FileType("r", encoding="UTF-8"),
+        help="Existing cascs file to merge with",
+    )
+    parser.add_argument(
         "outfile",
-        type=argparse.FileType("w", encoding="UTF-8"),
+        type=str,
         nargs="+",
         help="Destination file for the data",
     )
@@ -89,18 +98,48 @@ def main():
     )
     args = parser.parse_args()
 
-    cascs = {c["id"]: c for c in fetch_cascs(args.url, args.prefix)}
-    cascs = sorted(list(cascs.values()), key=lambda x: x.get("name", x.get("id")))
+    existing_cascs = {}
+    if args.infile.name.endswith("csv"):
+        reader = csv.DictReader(args.infile)
+        existing_cascs = {r["id"]: {**r, "active": False} for r in reader}
+        print(f"Loaded {len(existing_cascs)} existing cascs")
+    elif args.infile.name.endswith("json"):
+        existing_cascs = {
+            r["id"]: {**r, "active": False} for r in json.load(args.infile)["cascs"]
+        }
+        print(f"Loaded {len(existing_cascs)} existing cascs")
+    args.infile.close()
 
-    for f in args.outfile:
-        if f.name.endswith("csv"):
-            writer = csv.DictWriter(
-                f, fieldnames=["id"] + RECORD_KEYS, lineterminator="\n"
-            )
-            writer.writeheader()
-            writer.writerows(cascs)
-        elif f.name.endswith("json"):
-            json.dump({"cascs": cascs}, f, indent=4)
+    cascs = {
+        **existing_cascs,
+        **{c["id"]: {**c, "active": True} for c in fetch_cascs(args.url, args.prefix)},
+    }
+    cascs = sorted(list(cascs.values()), key=lambda x: x.get("name", x.get("id")))
+    print(f"Found {len(cascs)} cascs")
+
+    name_match = defaultdict(set)
+    for c in cascs:
+        name_match[c["name"]].add(c["id"])
+
+    with open("name_match.csv", "w") as f:
+        writer = csv.DictWriter(
+            f, fieldnames=["id1", "id2", "name"], lineterminator="\n"
+        )
+        writer.writeheader()
+        for k, v in name_match.items():
+            if len(v) == 2:
+                writer.writerow({"id1": list(v)[0], "id2": list(v)[1], "name": k})
+
+    for filename in args.outfile:
+        with open(filename, "w", encoding="UTF-8") as f:
+            if filename.endswith("csv"):
+                writer = csv.DictWriter(
+                    f, fieldnames=["id"] + RECORD_KEYS, lineterminator="\n"
+                )
+                writer.writeheader()
+                writer.writerows(cascs)
+            elif filename.endswith("json"):
+                json.dump({"cascs": cascs}, f, indent=4)
 
         f.close()
 
